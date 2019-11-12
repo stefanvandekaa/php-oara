f2df58b5548c8b13c3add4561c09344cf79bd3a1
Gert-Jaap Hertgers <info@trilectica.nl>
2019-11-12 21:34:32 +0100

    extend Daisycon

--- Oara/Network/Publisher/Daisycon.php
+++ Oara/Network/Publisher/Daisycon.php
@@ -177,6 +177,7 @@ class Daisycon extends \Oara\Network
 		$user = $this->_credentials['user'];
 		$password = $this->_credentials['password'];
 		$id_site = $this->_credentials['idSite'];      // publisher id to retrieve (empty = all publishers)
+        $mediaId = $this->_credentials['media_id'];      // media id to retrieve (empty = all media's)
 
 		foreach ($this->_publisherId as $publisherId) {
 			if (!empty($id_site) && $publisherId != $id_site) {
@@ -189,7 +190,7 @@ class Daisycon extends \Oara\Network
 			$finish = false;
 
 			while (!$finish) {
-				$url = "https://services.daisycon.com:443/publishers/$publisherId/transactions?page=$page&per_page=$pageSize&start=" . \urlencode($dStartDate->format("Y-m-d H:i:s")) . "&end=" . \urlencode($dEndDate->format("Y-m-d H:i:s"));
+				$url = "https://services.daisycon.com:443/publishers/$publisherId/transactions?media_id=$mediaId&page=$page&per_page=$pageSize&start=" . \urlencode($dStartDate->format("Y-m-d H:i:s")) . "&end=" . \urlencode($dEndDate->format("Y-m-d H:i:s"));
 				// initialize curl resource
 				$ch = curl_init();
 				// set the http request authentication headers
